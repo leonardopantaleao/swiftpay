9cfd118d53c50d81bbba08df8bf7ca8f3207c329
Leonardo Panta Leão <leonardopspl@gmail.com>
2020-08-17 15:41:44 -0300

    started ios project

---
+++ ios-swiftpay/iOSSwiftPay/iOSSwiftPay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSSwiftPay
+//
+//  Created by Leonardo on 17/08/20.
+//  Copyright © 2020 Leonardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
