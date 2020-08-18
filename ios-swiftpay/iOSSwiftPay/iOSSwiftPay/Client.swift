9d9b2701728a94d8fd3053ede5511c7329a42d6f
Leonardo Panta Leão <leonardopspl@gmail.com>
2020-08-17 22:54:38 -0300

    antes de implementar login

---
+++ ios-swiftpay/iOSSwiftPay/iOSSwiftPay/Client.swift
@@ -0,0 +1,16 @@
+//
+//  Client.swift
+//  iOSSwiftPay
+//
+//  Created by Leonardo on 17/08/20.
+//  Copyright © 2020 Leonardo. All rights reserved.
+//
+
+import Foundation
+
+
+struct Client: Codable{
+    var id: Int
+    var name: String
+    var cpf: String
+}
--- ios-swiftpay/iOSSwiftPay/iOSSwiftPay/ViewController.swift
+++ ios-swiftpay/iOSSwiftPay/iOSSwiftPay/ViewController.swift
@@ -8,13 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var clients = [Client]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        fetchData()
     }
 
-
+    func fetchData(){
+        let url = URL(string: "http://localhost:8080/clients")!
+        
+        URLSession.shared.dataTask(with: url) { data, response, error
+            in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "Erro desconhecido")
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            
+            if let clients = try? decoder.decode([Client].self, from: data){
+                DispatchQueue.main.async {
+                    self.clients = clients
+                    self.tableView.reloadData()
+                    print("Obteve \(clients.count) clientes.")
+                }
+            }
+            else {
+                print("Não foi possível fazer parse da resposta JSON.")
+            }
+        }.resume()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return clients.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let client = clients[indexPath.row]
+        
+        cell.textLabel?.text = "\(client.name)"
+        cell.detailTextLabel?.text = "\(client.cpf)"
+        
+        return cell
+    }
 }
 
