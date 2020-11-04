9396e95e98ad1cd80e8740369b78f32d64a0df09
Leonardo Panta Leão <leonardopspl@gmail.com>
2020-11-04 18:51:22 -0300

    removido vapor desnecessário

--- vapor-clean/.build/x86_64-apple-macosx/debug/CNIOLinux.build/ifaddrs-android.c.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/leonardo/Documents/SwiftPay/vapor-clean/.build/checkouts/swift-nio/Sources/CNIOLinux/ifaddrs-android.c
--- vapor-clean/migrations.swift
+++
@@ -1,8 +0,0 @@
-//
-//  migrations.swift
-//  VaporApp
-//
-//  Created by Leonardo on 23/09/20.
-//
-
-import Foundation
